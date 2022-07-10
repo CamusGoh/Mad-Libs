f6a63efc4a9c52e45b4fd1733c13bbbd8ff95524
Camus Goh <109012339+CamusGoh@users.noreply.github.com>
2022-07-10 17:42:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# This will be a Mad Lib game
+
+print("Hello\nI haven't see you before, what is your name?")
+user_name = input("Enter your name: ")
+print("")
+
+print("That's a very nice name " + user_name)
+print("Since you're here, let's play a game of Mad Lib!")
+print("This will be the template")
+print("Roses are [1]____\nViolets are [2]____\nI love [3]____\nAnd so do you\nThere will be [4]____ but do not cry")
+print("As there is no such thing as a [5]____ try")
+
+print("")
+Q1 = input("[1] Enter a colour: ")
+Q2 = input("[2] Enter another colour: ")
+Q3 = input("[3] Enter a verb: ")
+Q4 = input("[4] Enter a noun: ")
+Q5 = input("[5] Enter a adverb: ")
+print("")
+
+print("Roses are " + Q1 + "\nViolets are " + Q2 + "\nI love " + Q3 + "\nAnd so do you")
+print("There will be " + Q4 + " but do not cry\nAs there is no such thing as a " + Q5 + " try")
+print("")
+
+print("Good job at creating your very own poem!")
+
+
+# Through this simple project, I have learned how to better use "print" and "input" function and how it can be used to
+# make games
+
+
